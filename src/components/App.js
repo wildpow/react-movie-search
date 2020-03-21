6f9130921dc593a5f057aa38430ddedc048b6343
wildpow <element1028@gmail.com>
2020-03-20 23:38:44 -0700

    Building out App.js, Movie.js, and Search.js. Also added API key to .env file

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./Header";
+import Movie from "./Movie";
+import Search from "./Search";
+
+const MOVIE_API_URL = "http://www.omdbapi.com/?i=";
+const INITAL_SEARCH = "tt3896198";
+const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    fetch(`${MOVIE_API_URL}${INITAL_SEARCH}${API_KEY}`)
+      .then(res => res.json())
+      .then(jsonRes => {
+        setMovies(jsonRes.Search);
+        setLoading(false);
+      });
+  }, []);
+
+  const search = searchValue => {
+    setLoading(true);
+    setErrorMessage(null);
+
+    fetch(`${MOVIE_API_URL}${searchValue}${API_KEY}`)
+      .then(res => res.json())
+      .then(jsonRes => {
+        if (jsonRes.Response === "True") {
+          setMovies(jsonRes.Search);
+          setLoading(false);
+        } else {
+          setErrorMessage(jsonRes.Error);
+          setLoading(false);
+        }
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header text="React Movie Search" />
+      <Search search={search} />
+      <p className="App-intro">Sharing a few of our favorite movies</p>
+      <div className="movies">
+        {loading && !errorMessage ? (
+          <span>loading...</span>
+        ) : errorMessage ? (
+          <div className="errorMessage">{errorMessage}</div>
+        ) : (
+          movies.map((movie, index) => (
+            <Movie key={`${index}=${movie.Title}`} movie={movie} />
+          ))
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const Search = props => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchInputChanges = e => {
+    setSearchValue(e.target.value);
+  };
+
+  const resetInputField = () => {
+    setSearchValue("");
+  };
+
+  const callSearchFunction = e => {
+    e.preventDefault();
+    props.search(searchValue);
+    resetInputField();
+  };
+
+  return (
+    <form className="search">
+      <input
+        type="text"
+        value={searchValue}
+        onChange={handleSearchInputChanges}
+      />
+      <input type="submit" onClick={callSearchFunction} value="SEARCH" />
+    </form>
+  );
+};
+
+export default Search;
